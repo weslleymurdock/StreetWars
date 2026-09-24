4b9274b3a208855e6da017ef4e1dde2d05352249
Weslley Murdock <weslleyluiz@outlook.com.br>
2026-09-24 03:06:03 -0300

    add projects

---
+++ src/StreetWars/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace StreetWars;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
